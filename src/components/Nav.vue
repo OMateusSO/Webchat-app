3fae9b22f29a5ef94087d4b951b1552e8249e550
Dali <mateuswy123@gmail.com>
2025-03-02 21:10:14 -0300

    nav ajuste

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -9,7 +9,7 @@
       </RouterLink>
 
       <!-- Menu para desktop -->
-      <div class="hidden sm:flex items-center space-x-4">
+      <div class="flex items-center space-x-4">
         <RouterLink
           to="/registrar"
           v-if="!isLoggedIn"
@@ -52,15 +52,6 @@
             </div>
           </div>
         </div>
-      </div>
-
-      <!-- Menu hamburguer para mobile -->
-      <div class="sm:hidden">
-        <button @click="toggleMobileMenu" class="text-gray-600 hover:text-indigo-600">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
-          </svg>
-        </button>
       </div>
     </nav>
 
